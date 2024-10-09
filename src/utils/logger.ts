886e6167d2cfe1e0257ced8f2d54194bddb1e57a
Manthan Sharma <mayank9870866130@gmail.com>
2024-10-09 19:56:17 +0530

    add linting fix

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -8,7 +8,7 @@ const logger = winston.createLogger({
   },
   format: winston.format.combine(
     winston.format.timestamp(),
-    winston.format.json()
+    winston.format.json(),
   ),
   transports: [
     new winston.transports.Console({
